d03c118c9eb69795eda0fbf3bd2535f04eee40d1
Monalize M <150396638+mlize08@users.noreply.github.com>
2025-11-17 19:41:41 -0300

    s

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ texto.textContent = "Monalize";
 
 let nome = prompt("Qual o seu nome?");
 if (texto.textContent == null){
-    texto.textContent ="Seja bem-vindo(a)!" 
+    texto.textContent ="Seja bem-vindo(a)!"; 
 }
 else{
     texto.textContent = nome;
